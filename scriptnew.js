36d829e2bf114e5e7a6579bfc5be55bda981733b
Yurii <example@gmail.com>
2022-05-12 14:06:07 +0300

    first commit

---
+++ scriptnew.js
@@ -0,0 +1,27 @@
+" use strict";
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt ("How is your name?","");
+// answers[1] = prompt ("How is your age?","");
+// answers[2] = prompt ("How is your city?","");
+
+// // document.write (answers);
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/4/5/6`);
+
+// const name = "Ivan";
+// console.log(`Привет,${name}`);
+
+console.log(4 + "5");
